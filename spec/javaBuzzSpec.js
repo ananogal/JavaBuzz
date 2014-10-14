13908f63d0080cdd6b17915e5df11ad0a514eafa
ananogal <ana.nogal@gmail.com>
2014-10-14 17:42:17 +0100

    finished

--- spec/javaBuzzSpec.js
+++ spec/javaBuzzSpec.js
@@ -1,9 +1,51 @@
 describe('JavaBuzz', function() {
-	
+	var javaBuzz = new JavaBuzz();
+
 	describe('number can be divided', function() {
 		it('by three', function() {
-			var javaBuzz = new JavaBuzz();
-			expect(javaBuzz.isDivisibleByThree(3)).toBe(true)
+			expect(javaBuzz.isDivisibleByThree(3)).toBe(true);
+		});
+
+		it('by five', function() {
+			expect(javaBuzz.isDivisibleByFive(5)).toBe(true);
+		});
+
+		it('by fifteen', function (){
+			expect(javaBuzz.isDivisibleByFifteen(15)).toBe(true);
+		});
+	});
+
+	describe('number can not be divided', function() {
+		it('by three', function() {
+			expect(javaBuzz.isDivisibleByThree(1)).toBe(false);
+		});
+
+		it('by five', function() {
+			expect(javaBuzz.isDivisibleByFive(1)).toBe(false);		
+		});
+
+		it('by fifteen', function(){
+			expect(javaBuzz.isDivisibleByFifteen(1)).toBe(false);
+		});
+	});
+
+	describe('can shout Fizz, Buzz and both', function(){
+		it('shouts Fizz', function () {
+			expect(javaBuzz.shout(3)).toEqual("Fizz");
+		});
+
+		it('shouts Buzz', function() {
+			expect(javaBuzz.shout(5)).toEqual("Buzz");
+		});	
+
+		it ('shouts FizzBuzz', function() {
+			expect(javaBuzz.shout(15)).toEqual("FizzBuzz");
+		});
+	});	
+
+	describe('shouts a number', function() {
+		it('when its not divisible by 3, 5, or 15', function() {
+			expect(javaBuzz.shout(2)).toEqual(2);
 		});
 	});
 });
--- src/javaBuzz.js
+++ src/javaBuzz.js
@@ -1,5 +1,24 @@
 function JavaBuzz(){}
 
 JavaBuzz.prototype.isDivisibleByThree = function(number) {
-	return true;
+	return this._isDivisibleBy(number, 3);
+};
+
+JavaBuzz.prototype.isDivisibleByFive = function(number) {
+	return this._isDivisibleBy(number, 5);
+};
+
+JavaBuzz.prototype.isDivisibleByFifteen = function(number) {
+	return this._isDivisibleBy(number, 15);
+};
+
+JavaBuzz.prototype._isDivisibleBy = function(number, divisor) {
+	return (number % divisor === 0);
+};
+
+JavaBuzz.prototype.shout = function(number) {
+	if (this.isDivisibleByFifteen(number)) return "FizzBuzz";
+	if (this.isDivisibleByThree(number)) return "Fizz";
+	if (this.isDivisibleByFive(number)) return "Buzz";
+	return number;
 };
